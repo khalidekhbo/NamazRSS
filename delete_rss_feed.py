3161bf4be666b30715608a99b8154bde69ac7cc7
khalidekhbo <112927910+khalidekhbo@users.noreply.github.com>
2023-08-19 16:29:47 +0600

    Create delete_rss_feed.py

---
+++ delete_rss_feed.py
@@ -0,0 +1,9 @@
+import os
+
+# Check if the rss_feed.xml file exists and delete it if it does
+if os.path.exists("rss_feed.xml"):
+    os.remove("rss_feed.xml")
+    print("rss_feed.xml file deleted successfully")
+else:
+    print("rss_feed.xml file does not exist")
+
