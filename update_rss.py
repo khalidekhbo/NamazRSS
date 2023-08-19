2f7810e237d4bdfdff1b0a2aaeafebf8d59496fe
khalidekhbo <112927910+khalidekhbo@users.noreply.github.com>
2023-08-19 17:01:16 +0600

    Update update_rss.py

--- update_rss.py
+++ update_rss.py
@@ -20,7 +20,7 @@ def fetch_prayer_times():
 
     return timings
 
-def generate_rss_feed(prayer_timings,last_update_time):
+def generate_rss_feed(prayer_timings, last_update_time):
     rss = Element("rss", version="2.0")
     channel = SubElement(rss, "channel")
 
@@ -28,7 +28,7 @@ def generate_rss_feed(prayer_timings,last_update_time):
     title.text = "Namaz Time"
 
     description = SubElement(channel, "description")
-    description.text = "Stay updated with daily Namaz timings.Last updated at {last_update_time} (Bangladesh Time)."
+    description.text = f"Stay updated with daily Namaz timings. Last updated at {last_update_time} (BDT)."
 
     for prayer, time in prayer_timings.items():
         item = SubElement(channel, "item")
@@ -40,15 +40,18 @@ def generate_rss_feed(prayer_timings,last_update_time):
 
     return tostring(rss)
 
-# Check if the rss_feed.xml file exists and delete it if it does
+# Check if the rss_feed.xml file exists and rename it if it does
 if os.path.exists("rss_feed.xml"):
-    os.remove("rss_feed.xml")
+    os.rename("rss_feed.xml", "old_rss_feed.xml")
+    print("rss_feed.xml file renamed to old_rss_feed.xml")
+else:
+    print("rss_feed.xml file does not exist")
 
 # Fetch prayer times
 prayer_times = fetch_prayer_times()
+current_time_bd = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() + 6 * 3600))  # Adding 6 hours for BDT
+rss_feed = generate_rss_feed(prayer_times, current_time_bd)
 
-current_time_bd = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() +6*3600))
-rss_feed = generate_rss_feed(prayer_times,current_time_bd)
 # Convert the bytes content to a file
 rss_feed_str = rss_feed.decode("utf-8")
 
