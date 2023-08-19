2d875c0ef9dd7fb7bb8238b671fea84279e18ba9
khalidekhbo <112927910+khalidekhbo@users.noreply.github.com>
2023-08-19 16:46:29 +0600

    Update delete_rss_feed.py

--- delete_rss_feed.py
+++ delete_rss_feed.py
@@ -2,8 +2,13 @@ import os
 
 # Check if the rss_feed.xml file exists and delete it if it does
 if os.path.exists("rss_feed.xml"):
-    os.remove("rss_feed.xml")
-    print("rss_feed.xml file deleted successfully")
+    os.rename("rss_feed.xml", "old_rss_feed.xml")
+    
+    print("rss_feed.xml file renamed successfully")
 else:
     print("rss_feed.xml file does not exist")
 
+#  os.remove("rss_feed.xml")
+   # print("rss_feed.xml file deleted successfully")
+#else:
+ #   print("rss_feed.xml file does not exist")
